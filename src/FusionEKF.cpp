66699fc4646fc511524ede8e2cb75dceec1436ed
amarkaswan <amarkaswan@gmail.com>
2021-07-12 18:49:32 +0000

    first

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -166,7 +166,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // TODO: Laser updates
     ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
-	ekf_.Update(measurement_pack.raw_measurements_);
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
